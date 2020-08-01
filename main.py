2965e3f33bfec22c60e2a2361ecab68bedef3811
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-08-01 17:21:52 +0300

    csv to dict - done

---
+++ main.py
@@ -0,0 +1,4 @@
+from shows_to_mongo.csv_to_json import csv_to_dict
+
+shows_list = csv_to_dict()   # подготовили данные для работы с MongoDB
+
---
+++ shows_to_mongo/csv_to_json.py
@@ -0,0 +1,27 @@
+import csv
+
+
+def csv_to_dict(path='C:/Users/79055/PycharmProjects/ORM/data/artists.csv') -> list:
+	"""
+
+	:param path: путь к нашему файлу по умолчанию прописан в аргументы, но при желании можно обработать любой
+	список концертов с идентичной структурой
+	:return: возвращает список словарей по каждому мероприятию
+	"""
+	with open(path, encoding='utf-8') as f:
+		rows = csv.reader(f, delimiter=",")
+		shows_list = list(rows)
+
+	init_doc = dict.fromkeys(['_id', *shows_list.pop(0)], None)
+	shows_list_json = []
+	count = 0
+	for show in shows_list:
+		count += 1
+		temp_dict = init_doc.copy()
+		temp_dict['_id'] = count
+		temp_dict['Исполнитель'] = show[0]
+		temp_dict['Цена'] = show[1]
+		temp_dict['Место'] = show[2]
+		temp_dict['Дата'] = show[3]
+		shows_list_json.append(temp_dict)
+	return shows_list_json
